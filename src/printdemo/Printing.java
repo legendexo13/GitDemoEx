f01788b17e2610291ee3ff501fa07e208366b873
aschando <aschando@inlen8520023545.corp.capgemini.com>
2022-10-27 21:02:20 +0530

    First Commit

---
+++ src/printdemo/Printing.java
@@ -0,0 +1,10 @@
+package printdemo;
+
+public class Printing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("This command prints stuff");
+	}
+
+}

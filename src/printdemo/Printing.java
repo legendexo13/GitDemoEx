d859e18fa94bfc85e4f253128866394493ce607c
aschando <aschando@inlen8520023545.corp.capgemini.com>
2022-10-28 09:47:35 +0530

    Second commit

--- src/printdemo/Printing.java
+++ src/printdemo/Printing.java
@@ -5,6 +5,8 @@ public class Printing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("This command prints stuff");
+	    System.out.println("This is my 2nd command");
+	
 	}
 
 }
